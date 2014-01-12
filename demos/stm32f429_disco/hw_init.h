694dfc4bf7d26a93ee481cafd0071691f7cadfd0
Grzegorz Kostka <kostka.grzegorz@gmail.com>
2014-01-12 16:25:13 +0000

    Update USB demo.

--- demos/stm32f429_disco/hw_init.h
+++ demos/stm32f429_disco/hw_init.h
@@ -18,13 +18,15 @@
 
 #include <config.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 void hw_init(void);
 void hw_usb_process(void);
 bool hw_usb_connected(void);
+bool hw_usb_enum_done(void);
 
 void hw_led_red(bool on);
 void hw_led_green(bool on);
-
+uint32_t hw_get_ms(void);
 
 #endif /* HW_INIT_H_ */
