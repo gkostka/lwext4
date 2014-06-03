902a5091914e0c77c478101e4cdeaea84aff02e6
gkostka <kostka.grzegorz@gmail.com>
2014-06-03 10:59:40 +0000

    Build system improvement.

--- toolchain/cortex-m0.cmake
+++ toolchain/cortex-m0.cmake
@@ -4,5 +4,6 @@ set(CMAKE_SYSTEM_PROCESSOR cortex-m0)
 
 set(MCPU_FLAGS "-mthumb -mcpu=cortex-m0")
 set(VFP_FLAGS "")
+set(LD_FLAGS "-nostartfiles")
 
 include(${CMAKE_CURRENT_LIST_DIR}/common/arm-none-eabi.cmake)
