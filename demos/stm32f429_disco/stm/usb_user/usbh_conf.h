6f8e5589c31cb4261d6ad9f3d55f14b698f0e77e
Grzegorz Kostka <kostka.grzegorz@gmail.com>
2014-11-08 13:45:38 +0000

    Disable STM lib debugs

--- demos/stm32f429_disco/stm/usb_user/usbh_conf.h
+++ demos/stm32f429_disco/stm/usb_user/usbh_conf.h
@@ -43,7 +43,7 @@
 #define USBH_KEEP_CFG_DESCRIPTOR              0
 #define USBH_MAX_SIZE_CONFIGURATION           0x200
 #define USBH_MAX_DATA_BUFFER                  0x200
-#define USBH_DEBUG_LEVEL                      3
+#define USBH_DEBUG_LEVEL                      0
 #define USBH_USE_OS                           0
     
 /* Exported constants --------------------------------------------------------*/
