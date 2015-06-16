ee301d4abbb9e17aa0c0fdb0c750925da3e03305
gkostka <kostka.grzegorz@gmail.com>
2015-06-16 10:01:47 +0100

    clang-format: ext4_bcache

--- lwext4/ext4_bcache.c
+++ lwext4/ext4_bcache.c
@@ -42,16 +42,15 @@
 #include <string.h>
 #include <stdlib.h>
 
-
 int ext4_bcache_init_dynamic(struct ext4_bcache *bc, uint32_t cnt,
-    uint32_t itemsize)
+                             uint32_t itemsize)
 {
     ext4_assert(bc && cnt && itemsize);
 
     memset(bc, 0, sizeof(struct ext4_bcache));
 
     bc->data = malloc(cnt * itemsize);
-    if(!bc->data)
+    if (!bc->data)
         goto error;
 
     bc->cnt = cnt;
@@ -61,9 +60,9 @@ int ext4_bcache_init_dynamic(struct ext4_bcache *bc, uint32_t cnt,
 
     return EOK;
 
-    error:
+error:
 
-    if(bc->data)
+    if (bc->data)
         free(bc->data);
 
     memset(bc, 0, sizeof(struct ext4_bcache));
@@ -73,7 +72,7 @@ int ext4_bcache_init_dynamic(struct ext4_bcache *bc, uint32_t cnt,
 
 int ext4_bcache_fini_dynamic(struct ext4_bcache *bc)
 {
-    if(bc->data)
+    if (bc->data)
         free(bc->data);
 
     memset(bc, 0, sizeof(struct ext4_bcache));
@@ -81,16 +80,15 @@ int ext4_bcache_fini_dynamic(struct ext4_bcache *bc)
     return EOK;
 }
 
-
 int ext4_bcache_alloc(struct ext4_bcache *bc, struct ext4_block *b,
-    bool *is_new)
+                      bool *is_new)
 {
     uint32_t i;
     ext4_assert(bc && b && is_new);
 
     /*Check if valid.*/
     ext4_assert(b->lb_id);
-    if(!b->lb_id){
+    if (!b->lb_id) {
         ext4_assert(b->lb_id);
     }
 
@@ -103,9 +101,9 @@ int ext4_bcache_alloc(struct ext4_bcache *bc, struct ext4_block *b,
     for (i = 0; i < bc->cnt; ++i) {
 
         /*Check if block is already in cache*/
-        if(b->lb_id == bc->lba[i]){
+        if (b->lb_id == bc->lba[i]) {
 
-            if(!bc->refctr[i] && !bc->free_delay[i])
+            if (!bc->refctr[i] && !bc->free_delay[i])
                 bc->ref_blocks++;
 
             /*Update reference counter*/
@@ -122,24 +120,24 @@ int ext4_bcache_alloc(struct ext4_bcache *bc, struct ext4_block *b,
         }
 
         /*Best fit calculations.*/
-        if(bc->refctr[i])
+        if (bc->refctr[i])
             continue;
 
-        if(bc->free_delay[i])
+        if (bc->free_delay[i])
             continue;
 
         /*Block is unreferenced, but it may exist block with
          * lower usage marker*/
 
         /*First find.*/
-        if(cache_id == bc->cnt){
+        if (cache_id == bc->cnt) {
             cache_id = i;
             alloc_id = bc->lru_id[i];
             continue;
         }
 
         /*Next find*/
-        if(alloc_id <= bc->lru_id[i])
+        if (alloc_id <= bc->lru_id[i])
             continue;
 
         /*This block has lower alloc id marker*/
@@ -147,8 +145,7 @@ int ext4_bcache_alloc(struct ext4_bcache *bc, struct ext4_block *b,
         alloc_id = bc->lru_id[i];
     }
 
-
-    if(cache_id != bc->cnt){
+    if (cache_id != bc->cnt) {
         /*There was unreferenced block*/
         bc->lba[cache_id] = b->lb_id;
         bc->refctr[cache_id] = 1;
@@ -160,9 +157,8 @@ int ext4_bcache_alloc(struct ext4_bcache *bc, struct ext4_block *b,
 
         /*Statistics*/
         bc->ref_blocks++;
-        if(bc->ref_blocks > bc->max_ref_blocks)
+        if (bc->ref_blocks > bc->max_ref_blocks)
             bc->max_ref_blocks = bc->ref_blocks;
-
 
         /*Block needs to be read.*/
         *is_new = true;
@@ -171,12 +167,12 @@ int ext4_bcache_alloc(struct ext4_bcache *bc, struct ext4_block *b,
     }
 
     ext4_dprintf(EXT4_DEBUG_BCACHE,
-        "ext4_bcache_alloc: FAIL, unable to alloc block cache!\n");
+                 "ext4_bcache_alloc: FAIL, unable to alloc block cache!\n");
     return ENOMEM;
 }
 
-int ext4_bcache_free (struct ext4_bcache *bc, struct ext4_block *b,
-    uint8_t free_delay)
+int ext4_bcache_free(struct ext4_bcache *bc, struct ext4_block *b,
+                     uint8_t free_delay)
 {
     ext4_assert(bc && b);
 
@@ -190,14 +186,14 @@ int ext4_bcache_free (struct ext4_bcache *bc, struct ext4_block *b,
     ext4_assert(bc->refctr[b->cache_id]);
 
     /*Just decrease reference counter*/
-    if(bc->refctr[b->cache_id])
+    if (bc->refctr[b->cache_id])
         bc->refctr[b->cache_id]--;
 
-    if(free_delay)
+    if (free_delay)
         bc->free_delay[b->cache_id] = free_delay;
 
     /*Update statistics*/
-    if(!bc->refctr[b->cache_id] && !bc->free_delay[b->cache_id])
+    if (!bc->refctr[b->cache_id] && !bc->free_delay[b->cache_id])
         bc->ref_blocks--;
 
     b->lb_id = 0;
@@ -207,8 +203,6 @@ int ext4_bcache_free (struct ext4_bcache *bc, struct ext4_block *b,
     return EOK;
 }
 
-
-
 bool ext4_bcache_is_full(struct ext4_bcache *bc)
 {
     return (bc->cnt == bc->ref_blocks);
@@ -217,5 +211,3 @@ bool ext4_bcache_is_full(struct ext4_bcache *bc)
 /**
  * @}
  */
-
-
